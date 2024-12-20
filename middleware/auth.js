1b62409468a10b5468820135ca0c638d60ef40e8
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-21 00:29:54 +0500

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; 
+
+    if (token == null)
+        return res.status(401).json({message:"UnAuthorized!"});
+
+    jwt.verify(token, 'secret', (err, data) => {
+        if (err)
+            return res.status(401).json({message:"UnAuthorized!"});
+        console.log(data,"from JWT")
+        next();
+    });
+}
+
+module.exports = authenticateToken

5385fdb72b3072a64a9e16d00acd9819a9c4e993
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-21 13:30:35 +0500

    Update StudentRouter.js

--- router/StudentRouter.js
+++ router/StudentRouter.js
@@ -7,7 +7,7 @@ router.post('/Stu',controller.AddStudent)
 router.get('/Student_/:id',controller.SearchStudent)
 router.put('/students/:id',controller.updateStudent)
 router.delete('/STUDENT/:id',controller.DeleteStudent)
-router.post('/Login',authenticateToken, controller.Login)
+router.post('/login', controller.Login)
 router.post('/:id/borrow/:bookId', controller.borrowBook);
 router.post('/return/:bookId', controller.returnBook);
 router.get('/borrowedBooks/:id', controller.viewBorrowedBooks);

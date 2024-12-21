f474bb012caef11f7b15501c14380e9c50264ffb
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-21 15:21:00 +0500

    Update StudentModels.js

--- models/StudentModels.js
+++ models/StudentModels.js
@@ -9,9 +9,9 @@ const StudentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    // age: { type: Number },
-    // role: { type: String, required: true },
-    // createdAt: { type: Date, default: Date.now }
+    age: { type: Number },
+    role: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 const Student = mongoose.model('Student', StudentSchema);
 module.exports = Student

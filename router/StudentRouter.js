4303e3ca570092be6b2188590b50005ce179d243
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-21 12:56:24 +0500

    Update StudentRouter.js

--- router/StudentRouter.js
+++ router/StudentRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const authenticateToken = require('../middleware/auth');
-
 const router = express.Router()
 const controller = require('../controller/StudentController')
 
@@ -8,7 +7,7 @@ router.post('/Stu',controller.AddStudent)
 router.get('/Student_/:id',controller.SearchStudent)
 router.put('/students/:id',controller.updateStudent)
 router.delete('/STUDENT/:id',controller.DeleteStudent)
-router.post('/Login', controller.Login)
+router.post('/Login',authenticateToken, controller.Login)
 router.post('/:id/borrow/:bookId', controller.borrowBook);
 router.post('/return/:bookId', controller.returnBook);
 router.get('/borrowedBooks/:id', controller.viewBorrowedBooks);

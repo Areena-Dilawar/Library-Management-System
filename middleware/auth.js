44aa1301b8551bfd8ef1f8aa6d0728822e1cd2c5
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-21 14:06:53 +0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,13 +3,15 @@ const jwt = require('jsonwebtoken');
 function authenticateToken(req, res, next) {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1]; 
-
+     console.log(token);
     if (token == null)
         return res.status(401).json({message:"UnAuthorized!"});
 
-    jwt.verify(token, 'secret', (err, data) => {
-        if (err)
+    jwt.verify(token, 'Jsonweb1234', (err, data) => {
+        if (err){
+            console.log(err);
             return res.status(401).json({message:"UnAuthorized!"});
+        }
         console.log(data,"from JWT")
         next();
     });

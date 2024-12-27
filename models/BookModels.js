c5dfb052100225f8c3df8d0a2255cabf202d8c85
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-27 20:15:59 +0500

    Update BookModels.js

--- models/BookModels.js
+++ models/BookModels.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  available: { type: Boolean, default: true },
-  role: { type: String, required: true }
+  available: { type: Boolean, default: true }
 });
 
 const Book = mongoose.model('Book', bookSchema);

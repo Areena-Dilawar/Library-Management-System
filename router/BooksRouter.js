cef047f569160788d1b329223b7e5747ec45c8a3
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-27 15:33:56 +0500

    Update BooksRouter.js

--- router/BooksRouter.js
+++ router/BooksRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router()
+const authenticateToken = require('../middleware/auth');
 const controller = require('../controller/BookController')
 
 router.post('/book',controller.AddBook)
 router.get('/books/:id',controller.SearchBook)
 router.put('/Books/:id',controller.updateBook)
-router.delete('/Book_/:id',controller.DeleteBook)
+router.delete('/Book_/:id',authenticateToken,controller.DeleteBook)
 
 module.exports = router

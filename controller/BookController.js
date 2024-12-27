6cb288b178980ba038c9c636aff176cc84d3edc5
Areena-Dilawar <areenadilawar@gmail.com>
2024-12-27 15:33:48 +0500

    Update BookController.js

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,12 +1,16 @@
 const Book = require('../models/BookModels')
+
 const AddBook = async (req, res) => {
     try {
         const data = req.body;
+        // if ( req.user.role == "admin") {
+        //     return res.status(401).json({ message: "Access Denied!" });
+        // }
         const object = await Book.create(data);
         res.json({ "message": "object created successfully", object });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message:"Internal Server Error" });
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 const SearchBook = async (req, res) => {
@@ -16,12 +20,15 @@ const SearchBook = async (req, res) => {
         res.json({ "message": "object found", object });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message:"Internal Server Error" });
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 const updateBook = async (req, res) => {
     try {
         const id = req.params.id
+        // if (req.user.role !== "user") {
+        //     return res.status(401).json({ message: "Access Denied!" });
+        // }
         const object = req.body
         const updatedBook = await Book.findByIdAndUpdate(id, object, { new: true });
         if (updatedBook === null) {
@@ -30,20 +37,23 @@ const updateBook = async (req, res) => {
         res.status(200).send(updatedBook);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message:"Internal Server Error" });
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 const DeleteBook = async (req, res) => {
-    try{
+    try {
         const id = req.params.id
+        if (req.user.role !== "admin") {
+            return res.status(401).json({ message: "Access Denied!" });
+        }
         const DeletedBook = await Book.findByIdAndDelete(id);
         if (DeletedBook === null) {
             return res.status(404).send("Book not found");
         }
         res.status(200).send(DeletedBook);
-    }catch(err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({ message:"Internal Server Error" });
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 module.exports = {

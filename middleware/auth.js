e1e6aadc3269fc7690c3407bb64c599732270e77
Areena-Dilawar <areenadilawar@gmail.com>
2025-01-01 15:22:22 +0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,20 +2,29 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1]; 
-     console.log(token);
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
     if (token == null)
-        return res.status(401).json({message:"UnAuthorized!"});
+        return res.status(401).json({ message: "UnAuthorized!" });
 
     jwt.verify(token, 'Jsonweb1234', (err, data) => {
-        if (err){
+        if (err) {
             console.log(err);
-            return res.status(401).json({message:"UnAuthorized!"});
+            return res.status(401).json({ message: "UnAuthorized!" });
         }
-        console.log(data,"from JWT")
+        console.log(data, "from JWT")
         req.user = data;
         next();
     });
 }
 
-module.exports = authenticateToken
+function authorization(...allowedroles) {
+    return (req, res, next) => {
+        if (!req.user || !allowedroles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Forbidden!" });
+        }
+        next();
+    }
+}
+
+module.exports = { authenticateToken, authorization }

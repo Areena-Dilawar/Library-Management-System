a92bfb5e695172f5fff361e15b3735b4c67f8b64
Areena-Dilawar <areenadilawar@gmail.com>
2025-01-01 15:22:02 +0500

    Update StudentRouter.js

--- router/StudentRouter.js
+++ router/StudentRouter.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const authenticateToken = require('../middleware/auth');
+const {authenticateToken, authorization} = require('../middleware/auth');
 const router = express.Router()
 const controller = require('../controller/StudentController')
 
 router.post('/Stu',controller.AddStudent)
-router.get('/Student_/:id',authenticateToken,controller.SearchStudent)
-router.put('/students/:id',authenticateToken, controller.updateStudent)
-router.delete('/STUDENT/:id',authenticateToken,controller.DeleteStudent)
+router.get('/Student_/:id',authenticateToken, authorization("admin") ,controller.SearchStudent)
+router.put('/students/:id', controller.updateStudent)
+router.delete('/STUDENT/:id',authenticateToken, authorization("admin") ,controller.DeleteStudent)
 router.post('/login', controller.Login)
 router.post('/:id/borrow/:bookId', controller.borrowBook);
 router.post('/return/:bookId', controller.returnBook);
 router.get('/borrowedBooks/:id', controller.viewBorrowedBooks);
-
+router.post('/forgetpassword', controller.forgetPassword);
+router.post('/resetPassword', controller.resetPassword);
+router.post('/generateOTP', controller.generateOTP);
 module.exports = router
